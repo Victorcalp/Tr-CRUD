586ad157ca73e9f8ba0e4845d93dfb0cefe1a830
Victor Carapinheiro <victorcalp@outlook.com.br>
2022-10-21 20:34:37 -0300

    Iniciando Projeto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Treinando_Crud.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
